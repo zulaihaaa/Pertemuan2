0e0722718411fa7532f3cee1da7a6bc9274e60f4
Siti Zulaiha <rupatsiti12@gmail.com>
2022-03-24 11:13:11 +0700

    contoh pembuatan array berdasarkan template Array standard Kotlin

---
+++ src/main/kotlin/Template_Array.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    // Creates an Array<String> with sizes 5 and values ["0", "1", "4", "9", "16"]
+    val asc = Array(5) {
+            i -> (i * i).toString()
+    }
+    asc.forEach { println(it) }
+}
