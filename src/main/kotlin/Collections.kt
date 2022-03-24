1288a4fb1ac4191542b40996efe7e9ffd2a9abb3
Siti Zulaiha <rupatsiti12@gmail.com>
2022-03-24 11:34:27 +0700

    Contoh dari kode pemograman Colections

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,28 +1,10 @@
+fun printAll(strings: Collection<String>) {
+    for(s in strings) print("$s ")
+    println()
+}
 fun main(args: Array<String>) {
-    //N-dimensional arrays
-    var cinemas = arrayOf<Array<Array<Int>>>()
-    for (i in 0..2) {
-        var cinema = arrayOf<Array<Int>>()
-        for (j in 0..4) {
-            var array = arrayOf<Int>()
-            for (k in 0..4) {
-                array += 0
-            }
-            cinema += array
-        }
-        cinemas += cinema
-    }
-    cinemas[1][2][3] = 1 // the second-floor cinema, the third row, the fourth seat
-    var floor = 1
-    for (cinema in cinemas) {
-        println("Floor: $floor")
-        floor += 1
-        for (array in cinema) {
-            for (value in array) {
-                print("$value ")
-            }
-            println()
-        }
-        println("-----------------")
-    }
+    val stringList = listOf("one", "two", "one")
+    printAll(stringList)
+    val stringSet = setOf("one", "two", "three")
+    printAll(stringSet)
 }
